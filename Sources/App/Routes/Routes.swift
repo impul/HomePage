7177586ffc1700871db3e43c9ab7271c1dfccd48
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2017-10-27 12:24:29 +0300

    Add homepage,styles and favicon

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,24 +2,9 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
+        get("") { req in
+           return try self.view.make("HomePage/index.html")
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
         try resource("posts", PostController.self)
     }
 }
