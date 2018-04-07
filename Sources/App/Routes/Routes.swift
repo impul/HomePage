b5331f2d9d285cefc5533b6155470a619f7be93b
Pavlo Boiko <pavlo.bojkoo@gmail.com>
2018-04-08 00:06:14 +0300

    remove chat

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,47 +2,33 @@ import Vapor
 import Foundation
 import Dispatch
 
-let room = Room()
-
 extension Droplet {
     func setupRoutes() throws {
         get("") { req in
            return try self.view.make("HomePage/index.html")
         }
         
-        socket("chat") { req, ws in
-            var pingTimer: DispatchSourceTimer? = nil
-            var username: String? = nil
+        socket("ws") { req, ws in
+            print("New WebSocket connected: \(ws)")
             
-            pingTimer = DispatchSource.makeTimerSource()
-            pingTimer?.scheduleRepeating(deadline: .now(), interval: .seconds(25))
-            pingTimer?.setEventHandler { try? ws.ping() }
-            pingTimer?.resume()
-            
-            ws.onText = { ws, text in
-                let json = try JSON(bytes: text.makeBytes())
-                
-                if let u = json.object?["username"]?.string {
-                    username = u
-                    room.connections[u] = ws
-                    room.bot("\(u) has joined. 👋")
-                }
-                
-                if let u = username, let m = json.object?["message"]?.string {
-                    room.send(name: u, message: m)
+            // ping the socket to keep it open
+            try background {
+                while ws.state == .open {
+                    try? ws.ping()
+                    self.console.wait(seconds: 10) // every 10 seconds
                 }
             }
             
-            ws.onClose = { ws, _, _, _ in
-                pingTimer?.cancel()
-                pingTimer = nil
+            ws.onText = { ws, text in
+                print("Text received: \(text)")
                 
-                guard let u = username else {
-                    return
-                }
-                
-                room.bot("\(u) has left")
-                room.connections.removeValue(forKey: u)
+                // reverse the characters and send back
+                let rev = String(text.reversed())
+                try ws.send(rev)
+            }
+            
+            ws.onClose = { ws, code, reason, clean in
+                print("Closed.")
             }
         }
         
